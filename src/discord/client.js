6b8f575beaddb73af5d5b2d006fe45e873cb9bfe
Jakub <orkaso69@gmail.com>
2023-06-01 17:30:42 +0200

    chore: log caught errors, parse commands cache

--- src/discord/client.js
+++ src/discord/client.js
@@ -21,7 +21,7 @@ function getClient(commandsCollection) {
 				return handleGameInteraction(interaction);
 			}
 		} catch (error) {
-			// console.error(error);
+			console.error(error);
 			if (interaction.replied || interaction.deferred) {
 				await interaction.followUp({
 					content: 'There was an error while executing this command!',
--- src/discord/registerCommands.js
+++ src/discord/registerCommands.js
@@ -3,7 +3,9 @@ import path from 'path';
 import { REST, Routes, Collection } from 'discord.js';
 
 const COMMANDS_CACHE_FILE = './commands_cache.json';
-const COMMANDS_CACHE = fs.existsSync(COMMANDS_CACHE_FILE) ? fs.readFileSync(COMMANDS_CACHE_FILE).toString() : null;
+const COMMANDS_CACHE = fs.existsSync(COMMANDS_CACHE_FILE)
+	? JSON.parse(fs.readFileSync(COMMANDS_CACHE_FILE).toString())
+	: null;
 
 async function registerSlashCommands(clientId, token) {
 	const COMMANDS_DIR = path.resolve(process.cwd(), 'src', 'commands');
@@ -23,7 +25,7 @@ async function registerSlashCommands(clientId, token) {
 		commands.push(command.data.toJSON());
 	}
 
-	if (!COMMANDS_CACHE || JSON.stringify(commands) !== COMMANDS_CACHE) {
+	if (!COMMANDS_CACHE || JSON.stringify(commands) !== JSON.stringify(COMMANDS_CACHE)) {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 		const data = await rest.put(Routes.applicationCommands(clientId), {
 			body: commands,
